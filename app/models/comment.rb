f4655a8657fd5b0c1dd0d28bc66241b4bd7dc813
gskalra <gurpalsinghkalra@gmail.com>
2021-09-13 23:39:46 +0800

    Remove status

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,4 @@ class Comment < ApplicationRecord
 include Visible
   belongs_to :question
 
-  VALID_STATUSES = ['public', 'private', 'archived']
-
-  validates :status, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
